e6a4ac42a05063b04f2166a7a37710c546424f17
Ivan Reutenko <reutenkoivan@gmail.com>
2023-08-01 09:58:57 +0300

    prettier

--- src/arrays/forEach/forEach.js
+++ src/arrays/forEach/forEach.js
@@ -1,5 +1,5 @@
 export const forEach = (array, callback, startIndex = 0) => {
-  for (let i = startIndex; i < array.length; i++) {
-    callback(array[i], i, array);
-  }
+	for (let i = startIndex; i < array.length; i++) {
+		callback(array[i], i, array)
+	}
 }
